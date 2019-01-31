b19b87ba519e9e6ef55990dbeff78fd0d38e5ade
Dana Prakoso <danaossoftware@gmail.com>
2019-01-31 16:41:15 +0700

    commit

--- confirm-email.php
+++ confirm-email.php
@@ -6,6 +6,7 @@ $results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
     $row = $results->fetch_assoc();
     if ($row["confirm_code"] == $confirmCode) {
+		$c->query("UPDATE users SET confirmed=1 WHERE email='" . $email . "'");
         echo 0;
     } else {
         echo -1;
