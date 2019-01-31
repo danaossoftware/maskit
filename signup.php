db49fcb663d3c45af198e940a12c6953e732ac3f
Dana Prakoso <danaossoftware@gmail.com>
2019-01-31 17:43:18 +0700

    commit

--- signup.php
+++ signup.php
@@ -24,11 +24,6 @@ $c->query("INSERT INTO users (id, email, phone, password, name, confirm_code) VA
 sendMail($email, "Konfirmasi email Maskit", "<html>
 	<head>
 		<style>
-		@font-face {
-			font-family: 'Palanquin';
-			src: url('http://sainsgo.com/maskit/Palanquin.ttf');
-		}
-		
 		html {
 			width: 100%;
 			height: 100%;
@@ -39,26 +34,17 @@ sendMail($email, "Konfirmasi email Maskit", "<html>
 			height: 100%;
 			overflow-x: hidden;
 			margin: 0 auto;
-			font-family: 'Palanquin', Arial;
+			font-family: Arial;
 		}
 		</style>
 	</head>
 	<body>
-		<div style=\"width: 100%; display: flex; flex-flow: column nowrap; position: relative;\">
-			<img style=\"position: absolute; left: 0; top: 0;\" width=\"100%\" height=\"400px\" src=\"http://sainsgo.com/maskit/mail_bg.jpg\">
-			<div style=\"margin-top: 50px; position: absolute; left: 0; top: 0; width: 100%; display: flex; flex-flow: column nowrap; align-items: center;\">
-				<div style=\"color: white; font-size: 16px;\">Konfirmasi email Maskit</div>
-				<div style=\"color: white; font-size: 30px; margin-top: 10px; margin-left: 50px; margin-right: 50px; text-align: center;\">Konfirmasikan email Anda untuk menyelesaikan pendaftaran Maskit</div>
-				<div style=\"width: calc(100% - 120px); margin-left: 30px; margin-right: 30px; background-color: white; padding: 30px; margin-top: 20px;\">
 					<div style=\"color: #888888; width: 100%; display: flex; justify-content: center; text-align: center;\">
 						Masukkan kode konfirmasi Maskit berikut untuk menyelesaikan pendaftaran Maskit. Konfirmasi ini hanya dilakukan sekali, untuk memastikan bahwa email yang Anda kirimkan adalah email yang sebenarnya. Jangan balas pesan ini. Jika email ini bukan untuk Anda, abaikan email ini.
 					</div>
 					<div style=\"width: 100%; display: flex; justify-content: center; color: #06c7a8; font-size: 40px; font-weight: bold;\">
 						0 1 7 B
 					</div>
-				</div>
-			</div>
-		</div>
 	</body>
 </html>");
 echo 0;
