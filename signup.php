fcb8b577cc22e778ef9d20fd9a363c76a743409f
Dana Prakoso <danaossoftware@gmail.com>
2019-01-31 17:15:45 +0700

    commit

--- signup.php
+++ signup.php
@@ -20,5 +20,5 @@ $confirmCode = substr($confirmCode, 0, 4);
 $confirmCode = strtoupper($confirmCode);
 $userId = uniqid();
 $c->query("INSERT INTO users (id, email, phone, password, name, confirm_code) VALUES ('" . $userId . "', '" . $email . "', '" . $phone . "', '" . $password . "', '" . $name . "', '" . $confirmCode . "')");
-sendMail($email, "Konfirmasi email Maskit", "Masukkan kode konfirmasi untuk menyelesaikan pendaftaran aplikasi Maskit berikut:<br/><br/><span style='font-size: 30px; font-weight: bold; color: black;'>"+$confirmCode+"</span>");
+sendMail($email, "Konfirmasi email Maskit", "Masukkan kode konfirmasi untuk menyelesaikan pendaftaran aplikasi Maskit berikut:<br/><br/><span style='font-size: 30px; font-weight: bold; color: black;'>" . $confirmCode . "</span>");
 echo 0;
