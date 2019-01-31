b6f0c97ed31289858ead708f01e638784054be3d
Dana Prakoso <danaossoftware@gmail.com>
2019-02-01 06:22:57 +0700

    commit

--- get-confirm-code.php
+++ get-confirm-code.php
@@ -2,10 +2,13 @@
 include 'db.php';
 include 'common.php';
 $email = $_GET["email"];
+echo $email . "<br/>";
 $results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
     $row = $results->fetch_assoc();
-    echo "{\"confirm_code\": \"" . $row["confirm_code"] . "\"}");
+	echo $row["confirm_code"];
+    //echo "{\"confirm_code\": \"" . $row["confirm_code"] . "\"}");
 } else {
-    echo returnCode(-1);
+	echo -1;
+    //echo returnCode(-1);
 }
