12d04c0f47109da348017513f9c628c74181e040
Dana Prakoso <danaossoftware@gmail.com>
2019-01-31 17:02:34 +0700

    commit

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 include 'db.php';
 $email = $_GET["email"];
 $password = $_GET["password"];
+$rememberMe = $_GET["remember_me"];
 $results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
     $row = $results->fetch_assoc();
@@ -14,6 +15,10 @@ if ($results && $results->num_rows > 0) {
         }
         session_start();
         $_SESSION["maskit_user_id"] = $row["id"];
+		if ($rememberMe == "true") {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), $_COOKIE[session_name()], time() + 60*60*24*14, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+		}
         echo 0;
     }
 } else {
