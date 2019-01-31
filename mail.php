216926919bd5bbb7f1d522b07040105249bb24b9
Dana Prakoso <danaossoftware@gmail.com>
2019-01-31 17:20:57 +0700

    commit

--- mail.php
+++ mail.php
@@ -5,6 +5,7 @@ function sendMail($dst, $subject, $msg) {
 	$message = $msg;
 	$headers = 'From: yolla@sainsgo.com' . "\r\n" .
 		'Reply-To: yolla@sainsgo.com' . "\r\n" .
+        "Content-type: text/html; charset=utf-8" . "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
 	mail($to, $subject, $message, $headers);
 }
