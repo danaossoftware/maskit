ba5c65c24653960a70cc373a1a0e127d456e6561
Dana Prakoso <danaossoftware@gmail.com>
2019-02-04 14:29:56 +0700

    commit

--- get-nearest-users.php
+++ get-nearest-users.php
@@ -3,7 +3,7 @@ include 'db.php';
 $diseaseId = $_GET["disease-id"];
 $latitude = floatval($_GET["latitude"]);
 $longitude = floatval($_GET["longitude"]);
-$results = $c->query("SELECT *, SQRT(POW(69.1 * (latitude - -7.2756141), 2) + POW(69.1 * (112.6416433 - longitude) * COS(latitude / 57.3), 2)) AS distance FROM users HAVING distance < 25 ORDER BY distance;");
+$results = $c->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $latitude . "), 2) + POW(69.1 * (" . $longitude . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM users HAVING distance < 25 ORDER BY distance;");
 $users = [];
 while ($row = $results->fetch_assoc()) {
 	array_push($users, $row);
