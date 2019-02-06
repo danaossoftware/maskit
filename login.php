285dcfbbe8c7964cc8bcd1b0e3e2d90316e0db92
Dana Prakoso <danaossoftware@gmail.com>
2019-02-07 06:02:17 +0700

    commit

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ include 'common.php';
 $email = $_POST["email"];
 $password = $_POST["password"];
 $rememberMe = $_POST["remember_me"];
-$deviceId = $_POST["device_id"];
+//$deviceId = $_POST["device_id"];
 $results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
     $row = $results->fetch_assoc();
@@ -15,9 +15,12 @@ if ($results && $results->num_rows > 0) {
             echo returnCode(-3);
             return;
         }
-		$ip = $_SERVER["REMOTE_ADDR"];
+		/*$ip = $_SERVER["REMOTE_ADDR"];
 		$c->query("INSERT INTO sessions (id, ip, user_id, last_active, device_id) VALUES ('" . uniqid() . "', '" . $ip . "', '" . $row["id"] . "', " . round(microtime(true)*1000) . ", '" . $deviceId . "')");
-        echo returnCode(0);
+        echo returnCode(0);*/
+		session_start();
+		$_SESSION["maskit_user_id"] = $row["id"];
+		echo returnCode(0);
     }
 } else {
     echo returnCode(-1);
