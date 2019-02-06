1e29dc730590ef519662954796a2e029f2edd4bc
Dana Prakoso <danaossoftware@gmail.com>
2019-02-06 10:22:05 +0700

    commit

--- get-points.php
+++ get-points.php
@@ -2,7 +2,7 @@
 include 'db.php';
 include 'common.php';
 $userId = getUserID();
-$results = $c->query("SELECT * FROM points WHERE user_id='5c538505c9f45'");
+$results = $c->query("SELECT * FROM points WHERE user_id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
 	$totalPoints = 0;
 	while ($row = $results->fetch_assoc()) {
