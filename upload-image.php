5fb375fc85b4e14e948f073fe906d717d6f9b930
Dana Prakoso <danaossoftware@gmail.com>
2019-02-06 10:14:26 +0700

    commit

--- upload-image.php
+++ upload-image.php
@@ -1,6 +1,3 @@
 <?php
 $fileName = $_POST["file-name"];
-if (!file_exists("userdata/imgs")) {
-	mkdir("userdata/imgs", 777);
-}
 move_uploaded_file($_FILES["uploaded_image"]["tmp_name"], "userdata/imgs/" . $fileName);
