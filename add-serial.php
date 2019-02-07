2a55e64eeb6f26668289a7b2905d146d6186d098
Dana Prakoso <danaossoftware@gmail.com>
2019-02-07 07:35:49 +0700

    commit

---
+++ add-serial.php
@@ -0,0 +1,5 @@
+<?php
+include 'db.php';
+$serial = $_POST["serial"];
+$c->query("INSERT INTO serials (id, serial) VALUES ('" . uniqid() . "', '" . $serial . "'");
+echo 0;
--- add-serials.php
+++ add-serials.php
@@ -2,5 +2,6 @@
 $arr = json_decode('[{"var1":"9","var2":"16","var3":"16"},{"var1":"8","var2":"15","var3":"15"}]');
 foreach($arr as $item) { //foreach element in $arr
 	echo "Fucking value...";
-	echo "a<br/>";
+    $uses = $item['var1']; //etc
+	echo $uses . "a<br/>";
 }
