e2293d4761ac8e05ad0cd5825442a96ffc42c426
Dana Prakoso <danaossoftware@gmail.com>
2019-02-07 20:38:14 +0700

    commit

--- report-condition.php
+++ report-condition.php
@@ -1,6 +1,8 @@
 <?php
 include 'db.php';
 include 'common.php';
-$totalPoints = $_GET["total_points"];
+$totalPoints = intval($_POST["total_points"]);
+$imgURL = $_POST["img_url"];
+$imgURL = utf8_decode(urldecode($imgURL));
 $userId = getUserID();
-$c->query("INSERT INTO points (id, user_id, points) VALUES ('" . uniqid() . "', '" . $userId . "', " . $totalPoints . ")");
+$c->query("INSERT INTO reports (id, user_id, proof, points) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $imgURL . "', " . $totalPoints . ")");
