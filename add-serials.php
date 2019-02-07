f6975d39eb019f867c185ef3b93a8e5222e7e8e1
Dana Prakoso <danaossoftware@gmail.com>
2019-02-07 07:30:08 +0700

    commit

--- add-serials.php
+++ add-serials.php
@@ -1,6 +1,7 @@
 <?php
 $arr = json_decode('[{"var1":"9","var2":"16","var3":"16"},{"var1":"8","var2":"15","var3":"15"}]');
 foreach($arr as $item) { //foreach element in $arr
+	echo "Fucking value...";
     $uses = $item['var1']; //etc
 	echo $uses . "<br/>";
 }
