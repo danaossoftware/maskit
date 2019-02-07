4481cbd0455e187e365c75cfa6ce64156484815e
Dana Prakoso <danaossoftware@gmail.com>
2019-02-07 07:26:02 +0700

    commit

--- add-serials.php
+++ add-serials.php
@@ -1,9 +1,5 @@
 <?php
-$json = '{
-    "title": "JavaScript: The Definitive Guide",
-    "author": "David Flanagan",
-    "edition": 6
-}';
+$json = file_get_contents("serial.json");
 $book = json_decode($json);
 // access title of $book object
-echo $book->title;
+echo $book[0]->serial;
