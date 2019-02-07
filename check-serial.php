e5811d58c6819c65da018d8f2dbd49463a461158
Dana Prakoso <danaossoftware@gmail.com>
2019-02-07 08:00:17 +0700

    commit

--- check-serial.php
+++ check-serial.php
@@ -2,4 +2,8 @@
 include 'db.php';
 $code = $_GET["code"];
 $results = $c->query("SELECT * FROM serials WHERE serial='" . $code . "'");
-echo $code;
+if ($results && $results->num_rows > 0) {
+	echo 0;
+} else {
+	echo -1;
+}
