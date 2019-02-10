cb6356f4e43da64f7c7dab6c789f89db7eedcd0e
Dana Prakoso <danaossoftware@gmail.com>
2019-02-10 09:38:10 +0700

    commit

--- add-user-serial.php
+++ add-user-serial.php
@@ -4,7 +4,7 @@ $maskCode = $_POST["mask_code"];
 $filterCode = $_POST["filter_code"];
 session_start();
 $userId = $_SESSION["maskit_user_id"];
-if ($filterCode != "") {
+if (!empty($filterCode)) {
 	$results = $c->query("SELECT * FROM user_serials WHERE user_id='" . $userId . "' AND filter_code='" . $filterCode . "'");
 	if ($results && $results->num_rows > 0) {
 		echo -1;
