f5d633ce571c29b81a15153a957a85e9925a13c3
Dana Prakoso <danaossoftware@gmail.com>
2019-02-10 10:39:56 +0700

    commit

--- input-filter.php
+++ input-filter.php
@@ -1,7 +1,6 @@
 <?php
 include 'db.php';
 include 'common.php';
-$maskCode = $_POST["mask-code"];
 $filterCode = $_POST["filter-code"];
 $totalPoints = $_POST["total_points"];
 $imgURL = $_POST["img_url"];
@@ -10,6 +9,11 @@ $descr = $_POST["desc"];
 session_start();
 $userId = $_SESSION["maskit_user_id"];
 $currentMillis = round(microtime(true)*1000);
+$results = $c->query("SELECT * FROM user_serials WHERE filter_code='" . $filterCode . "'");
+if ($results && $results->num_rows > 0) {
+	echo -1;
+	return;
+}
 $c->query("INSERT INTO reports (id, user_id, proof, points, date, descr) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $imgURL . "', " . $totalPoints . ", " . $currentMillis . ", '" . $descr . "')");
-$c->query("INSERT INTO lifetime (id, mask_id, filter_id, user_id, start_time) VALUES ('" . uniqid() . "', '" . $maskCode . "', '" . $filterCode . "', '" . $userId . "', " . round(microtime(true) * 1000) . ")");
+$c->query("INSERT INTO lifetime (id, filter_id, user_id, start_time) VALUES ('" . uniqid() . "', '" . $filterCode . "', '" . $userId . "', " . round(microtime(true) * 1000) . ")");
 echo 0;
---
+++ input-mask.php
@@ -0,0 +1,19 @@
+<?php
+include 'db.php';
+include 'common.php';
+$maskCode = $_POST["mask-code"];
+$totalPoints = $_POST["total_points"];
+$imgURL = $_POST["img_url"];
+$imgURL = utf8_decode(urldecode($imgURL));
+$descr = $_POST["desc"];
+session_start();
+$userId = $_SESSION["maskit_user_id"];
+$currentMillis = round(microtime(true)*1000);
+$results = $c->query("SELECT * FROM user_serials WHERE mask_code='" . $maskCode . "'");
+if ($results && $results->num_rows > 0) {
+	echo -1;
+	return;
+}
+$c->query("INSERT INTO reports (id, user_id, proof, points, date, descr) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $imgURL . "', " . $totalPoints . ", " . $currentMillis . ", '" . $descr . "')");
+$c->query("INSERT INTO lifetime (id, mask_id, user_id, start_time) VALUES ('" . uniqid() . "', '" . $maskCode . "', '" . $userId . "', " . round(microtime(true) * 1000) . ")");
+echo 0;
