23a514ac568345b62daa54ea130a2b263d45ff86
Dana Prakoso <danaossoftware@gmail.com>
2019-02-12 14:37:07 +0700

    commit

---
+++ get-masks-by-points.php
@@ -0,0 +1,11 @@
+<?php
+include 'db.php';
+$points = intval($_POST["points"]);
+$results = $c->query("SELECT * FROM masks WHERE points >= " . $points);
+$masks = [];
+if ($results && $results->num_rows > 0) {
+	while ($row = $results->fetch_assoc()) {
+		array_push($masks, $row);
+	}
+}
+echo json_encode($masks);
--- get-rewards.php
+++ get-rewards.php
@@ -1,6 +1,6 @@
 <?php
 include 'db.php';
-$results = $c->query("SELECT * FROM rewards");
+$results = $c->query("SELECT * FROM masks");
 $rewards = [];
 if ($results && $results->num_rows > 0) {
 	while ($row = $results->fetch_assoc()) {
