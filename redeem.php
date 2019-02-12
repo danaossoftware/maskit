5f321c6a4c50661325345137f2869e7773b3857f
Dana Prakoso <danaossoftware@gmail.com>
2019-02-12 15:34:58 +0700

    commit

--- redeem.php
+++ redeem.php
@@ -1,5 +1,6 @@
 <?php
 include 'db.php';
+include 'mail.php';
 $fileName = $_POST["file_name"];
 $userId = $_POST["user_id"];
 $maskId = $_POST["mask_id"];
@@ -7,3 +8,4 @@ $date = round(microtime(true)*1000);
 $fileURL = "userdata/imgs/" . $fileName;
 move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], $fileURL);
 $c->query("INSERT INTO redeems (id, user_id, mask_id, date, img_url) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $maskId . "', " . $date . ", '" . $fileURL . "')");
+sendMail("sainsgokaryaindonesia@gmail.com", "Bukti penukaran poin masker/filter Maskit", "User dengan ID " . $userId . " telah menukar poin untuk membeli masker/filter dengan ID " . $maskId . ". Data bisa dicek di database.");
