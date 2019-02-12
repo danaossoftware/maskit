b43b9d52a245ade58747c5ca4c173b0bd0e4f1a1
Dana Prakoso <danaossoftware@gmail.com>
2019-02-12 15:28:44 +0700

    commit

--- redeem.php
+++ redeem.php
@@ -1,6 +1,9 @@
 <?php
 include 'db.php';
+$fileName = $_POST["file_name"];
 $userId = $_POST["user_id"];
 $maskId = $_POST["mask_id"];
 $date = round(microtime(true)*1000);
-$c->query("INSERT INTO redeems (id, user_id, mask_id, date) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $maskId . "', " . $date . ")");
+$fileURL = "userdata/imgs/" . $fileName;
+move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], $fileURL);
+$c->query("INSERT INTO redeems (id, user_id, mask_id, date, img_url) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $maskId . "', " . $date . ", '" . $fileURL . "')");
