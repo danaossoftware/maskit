131dc818a7c1ded9ef9af104c07bede89a02dc9b
Dana Prakoso <danaossoftware@gmail.com>
2019-02-12 16:03:57 +0700

    commit

--- get-points.php
+++ get-points.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 $userId = $_POST["user_id"];
-$results = $c->query("SELECT * FROM users WHERE user_id='" . $userId . "'");
+$results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
 	$row = $results->fetch_assoc();
 	$totalPoints = $row["points"];
