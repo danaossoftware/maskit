28c45f8d2fa43b5aa3e6c7779f9224b06c55ff5b
Dana Prakoso <danaossoftware@gmail.com>
2019-02-12 16:18:28 +0700

    commit

--- redeem.php
+++ redeem.php
@@ -9,7 +9,7 @@ $date = round(microtime(true)*1000);
 $fileURL = "http://sainsgo.com/maskit/userdata/imgs/" . $fileName;
 move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], "userdata/imgs/" . $fileName);
 $c->query("INSERT INTO redeems (id, user_id, mask_id, date, img_url) VALUES ('" . uniqid() . "', '" . $userId . "', '" . $maskId . "', " . $date . ", '" . $fileURL . "')");
-sendMail("danaoscompany@gmail.com", "Bukti penukaran poin masker/filter Maskit", "User dengan ID " . $userId . " telah menukar poin untuk membeli masker/filter dengan ID " . $maskId . ". Data bisa dicek di database.");
+sendMail("sainsgokaryaindonesia@gmail.com", "Bukti penukaran poin masker/filter Maskit", "User dengan ID " . $userId . " telah menukar poin untuk membeli masker/filter dengan ID " . $maskId . ". Data bisa dicek di database.");
 $results = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
 if ($results && $results->num_rows > 0) {
 	$row = $results->fetch_assoc();
