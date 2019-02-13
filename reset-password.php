660ec2e0412cdc75050f144c3e7df103a529bec2
Dana Prakoso <danaossoftware@gmail.com>
2019-02-13 22:15:28 +0700

    commit

--- reset-password.php
+++ reset-password.php
@@ -1,33 +1,10 @@
 <?php
-include 'mail.php';
+include 'db.php';
 $email = $_POST["email"];
-$confirmCode = md5(uniqid(rand(), true));
-$confirmCode = substr($confirmCode, 0, 4);
-$confirmCode = strtoupper($confirmCode);
-sendMail($email, "Atur ulang kata sandi Maskit", "<html>
-	<head>
-		<style>
-		html {
-			width: 100%;
-			height: 100%;
-		}
-		
-		body {
-			width: 100%;
-			height: 100%;
-			overflow-x: hidden;
-			margin: 0 auto;
-			font-family: Arial;
-		}
-		</style>
-	</head>
-	<body>
-					<div style=\"color: #888888;\">
-						Masukkan kode konfirmasi Maskit berikut untuk mengatur ulang kata sandi Maskit. Konfirmasi ini hanya dilakukan sekali, untuk memastikan bahwa email yang Anda kirimkan adalah email yang sebenarnya. Jangan balas pesan ini. Jika email ini bukan untuk Anda, abaikan email ini.
-					</div>
-					<div style=\"color: #888888; color: #06c7a8; font-size: 40px; font-weight: bold;\">
-						" . $confirmCode . "
-					</div>
-	</body>
-</html>");
-echo $confirmCode;
+$password = $_POST["password"];
+$c->query("UPDATE users SET password='" . $password . "' WHERE email='" . $email . "'");
+session_id("maskit");
+session_start();
+$_SESSION["maskit_user_id"] = "";
+unset($_SESSION["maskit_user_id"]);
+session_destroy();
