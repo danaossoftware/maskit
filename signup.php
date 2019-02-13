087fdb0d92b7362323f7196730f775e3e8501925
Dana Prakoso <danaossoftware@gmail.com>
2019-02-13 11:04:22 +0700

    commit

--- signup.php
+++ signup.php
@@ -8,7 +8,7 @@ $password = $_POST["password"];
 $name = $_POST["name"];
 $results = $c->query("SELECT * FROM users WHERE email='" . $email . "'");
 if ($results && $results->num_rows > 0) {
-	echo $email;
+	echo -1;
     return;
 }
 $results = $c->query("SELECT * FROM users WHERE phone='" . $phone . "'");
