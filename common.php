ef1c9f6318df0558fdddee5f53b7ef5e91456b40
Dana Prakoso <danaossoftware@gmail.com>
2019-02-17 15:28:21 +0700

    commit

--- common.php
+++ common.php
@@ -34,3 +34,18 @@ function getUserID() {
         return -1;
     }
 };
+
+function distance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000) {
+  // convert from degrees to radians
+  $latFrom = deg2rad($latitudeFrom);
+  $lonFrom = deg2rad($longitudeFrom);
+  $latTo = deg2rad($latitudeTo);
+  $lonTo = deg2rad($longitudeTo);
+
+  $latDelta = $latTo - $latFrom;
+  $lonDelta = $lonTo - $lonFrom;
+
+  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+    cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+  return $angle * $earthRadius;
+}
---
+++ get-nearest-disease.php
@@ -0,0 +1,24 @@
+<?php
+include 'db.php';
+include 'common.php';
+$diseaseId = $_POST["disease_id"];
+$latitude = doubleval($_POST["latitude"]);
+$longitude = doubleval($_POST["longitude"]);
+$results = $c->query("SELECT * FROM diseases WHERE disease_id='" . $diseaseId . "'");
+$diseases = [];
+if ($results && $results->num_rows > 0) {
+	while ($row = $results->fetch_assoc()) {
+		$userId = $row["user_id"];
+		$results2 = $c->query("SELECT * FROM users WHERE id='" . $userId . "'");
+		if ($results2 && $results2->num_rows > 0) {
+			$row2 = $results2->fetch_assoc();
+			$lat = $row2["latitude"];
+			$lng = $row2["longitude"];
+			$distance = distance($latitude, $longitude, $lat, $lng, 6371000);
+			if ($distance <= 25000) {
+				array_push($diseases, $row);
+			}
+		}
+	}
+}
+echo json_encode($diseases);
