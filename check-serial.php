450aeeb097e3e9faa295801fd0dd079c7cacd23c
Dana Prakoso <danaossoftware@gmail.com>
2019-02-18 00:38:19 +0700

    commit

--- check-serial.php
+++ check-serial.php
@@ -4,12 +4,13 @@ $code = $_GET["code"];
 $results = $c->query("SELECT * FROM serials WHERE serial='" . $code . "'");
 if ($results && $results->num_rows > 0) {
 	$row = $results->fetch_assoc();
-	$used = $row["used"];
+	/*$used = $row["used"];
 	if ($used == 0) {
 		echo 0;
 	} else {
 		echo -2;
-	}
+	}*/
+	echo 0;
 } else {
 	echo -1;
 }
