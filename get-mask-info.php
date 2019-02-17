83a2bd6c64943cb8733157cdd4c99a81973be063
Dana Prakoso <danaossoftware@gmail.com>
2019-02-18 00:27:40 +0700

    commit

--- get-mask-info.php
+++ get-mask-info.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
-$maskId = $_POST["mask_id"];
-$filterId = $_POST["filter_id"];
+$maskId = $_GET["mask_id"];
+$filterId = $_GET["filter_id"];
 $results = $c->query("SELECT * FROM masks WHERE mask_code='" . $maskId . "' OR filter_code='" . $filterId . "'");
 if (!$results || $results->num_rows <= 0) {
 	$results = $c->query("SELECT * FROM masks WHERE filter_code='" . $maskId . "' OR mask_code='" . $filterId . "'");
