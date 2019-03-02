3fd1673b2e3dde8cdc7374c1a9412d37ee628b16
Dana Prakoso <danaossoftware@gmail.com>
2019-03-02 15:34:32 +0700

    commit

--- set-location.php
+++ set-location.php
@@ -2,8 +2,9 @@
 include 'db.php';
 $latitude = doubleval($_POST["latitude"]);
 $longitude = doubleval($_POST["longitude"]);
+$address = $_POST["address"];
 session_id("maskit");
 session_start();
 $userId = $_SESSION["maskit_user_id"];
-$c->query("UPDATE users SET latitude=" . $latitude . ", longitude=" . $longitude . " WHERE id='" . $userId . "'");
+$c->query("UPDATE users SET latitude=" . $latitude . ", longitude=" . $longitude . ", address='" . $address . "' WHERE id='" . $userId . "'");
 echo 0;
